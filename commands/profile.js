1713a01ca3592dddf7d7721bc7b0802069a5934c
Victor Litvinkov <victor@sunfox.ee>
2023-03-17 15:05:00 +0200

    Profile page update

--- commands/profile.js
+++ commands/profile.js
@@ -25,7 +25,7 @@ module.exports = {
 
 		const hasAdminRole = interaction.member.roles.cache.some(r=>JSON.stringify(config.admin_roles).includes(r.name))
 		if (hasAdminRole == 'true') {
-			var data_user = interaction.options.getMember('target_user') == null ? interaction.member.user.id : interaction.options.getMember('target_user');
+			var data_user = interaction.options.getMember('target_user') == 'null' ? interaction.member.user.id : interaction.options.getMember('target_user');
 		} else {
 			var data_user = interaction.member.user.id ;
 		}
