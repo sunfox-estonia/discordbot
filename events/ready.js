8e111e658400b1f1de6e48a8474f45ecdd365ba9
Victor Litvinkov <victor@sunfox.ee>
2023-03-17 13:32:03 +0200

    Ready.js file update.

--- events/ready.js
+++ events/ready.js
@@ -1,14 +1,4 @@
 const { Events } = require('discord.js');
-const config = require('config.json');
-const mysql = require('mysql');
-const database = mysql.createConnection({
-    host: config.db_config.host,
-    user: config.db_config.dbuser,
-    password: config.db_config.dbpass,
-    database: config.db_config.dbname,
-    debug: false,
-    multipleStatements: true,
-  });
 
 module.exports = {
 	name: Events.ClientReady,
