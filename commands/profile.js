14f6d2618482050c96beeb65a2a0400467d1bf7c
Victor Litvinkov <victor@sunfox.ee>
2023-03-20 20:45:01 +0200

    Profile update

--- commands/profile.js
+++ commands/profile.js
@@ -35,7 +35,7 @@ module.exports = {
 			fetchedUser => {
 				var embed_profile = prepareEmbed(fetchedUser.user);
 				console.log(fetchedUser.user);
-				//interaction.reply({embeds: [embed_profile]});
+				interaction.reply({embeds: [embed_profile]});
 			}
 
 		).catch(console.error);	
@@ -48,7 +48,10 @@ function prepareEmbed(user_data) {
 	let sql1 = "SELECT drd_users.uid, drd_users.level, drd_users.coins, drd_levels.title, drd_levels.symbol FROM drd_users LEFT JOIN drd_levels ON drd_users.level = drd_levels.level WHERE drd_users.uid = ? LIMIT 1;";   
 	database.query(sql1, [user_data.id], (error1, result_userdata, fields) => {
 		if (error1) {
-			return console.log(error1.message);
+			return 'false';
+		}
+		if (result_userdata.length == 0){
+			return 'false';
 		}
 		if (result_userdata.length == 1){
 
@@ -58,39 +61,46 @@ function prepareEmbed(user_data) {
 			let sql2 = "SELECT drd_achievements.code, drd_achievements.title, drd_achievements.description, drd_usr_ach.date FROM drd_achievements LEFT JOIN drd_usr_ach ON drd_achievements.code = drd_usr_ach.ach_id AND drd_usr_ach.user_id = ? WHERE drd_achievements.level = ?;"; 
 			database.query(sql2, [result_userdata[0].uid, result_userdata[0].level], function(error2, result_levels, fields) {
 				if (error2) {
-					return console.log(error2.message);
+					return 'false';
 				} 
-			
-				var embed_profile = {
-					title: (String.fromCodePoint(result_levels[0].symbol) +' '+ result_levels[0].title),
-					description: user_source[0].level +' уровень | ' + user_source[0].coins + ' золотых',
-					color: 0x0099ff,
-					thumbnail: {
-						url: member_data.avatarURL
-					},
-					author: {
-						name: "Vitgor Sunfox"      
-					},
-					fields: [
+				if (result_levels.length == 0){
+                    return 'false';
+                }
+				if (result_levels.length == 1){
+
+					var embed_profile = {
+						title: (String.fromCodePoint(result_levels[0].symbol) +' '+ result_levels[0].title),
+						description: user_source[0].level +' уровень | ' + user_source[0].coins + ' золотых',
+						color: 0x0099ff,
+						thumbnail: {
+							url: member_data.avatarURL
+						},
+						author: {
+							name: "Vitgor Sunfox"      
+						},
+						fields: [
+							{
+							name: "\u200b",
+							value:"\u200b"
+						},
 						{
-						name: "\u200b",
-						value:"\u200b"
-					},
-					{
-						name: ":white_medium_square: - Невидимый убийца",
-						value: "С возвышенности 10 м. бросить LARP-копьё (сулицу), попав не менее 3 раз из 5 в размеченную зону диаметром 3 метра."
-					},
-					{
-						name: "\u200b",
-						value:"\u200b"
+							name: ":white_medium_square: - Невидимый убийца",
+							value: "С возвышенности 10 м. бросить LARP-копьё (сулицу), попав не менее 3 раз из 5 в размеченную зону диаметром 3 метра."
+						},
+						{
+							name: "\u200b",
+							value:"\u200b"
+						}
+						],
+						timestamp: new Date().toISOString(),
+						footer: {
+							icon_url: "https://sunfox.ee/resources/img/discord_bot/vv_sq_logo.png",
+							text: "Викинги Вирумаа"
+						},
 					}
-					],
-					timestamp: new Date().toISOString(),
-					footer: {
-						icon_url: "https://sunfox.ee/resources/img/discord_bot/vv_sq_logo.png",
-						text: "Викинги Вирумаа"
-					},
-				}
+
+					return embed_profile;
+				}	
 			});
 		}	
 	});	
