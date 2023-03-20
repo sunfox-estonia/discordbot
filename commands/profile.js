c4401a5c2e42dd195297aebfbfbfec11451d8e35
Victor Litvinkov <victor@sunfox.ee>
2023-03-20 20:58:00 +0200

    Profile update

--- commands/profile.js
+++ commands/profile.js
@@ -65,7 +65,7 @@ function prepareEmbed(user_data) {
                 }
 				if (result_levels.length == 1){
 
-					console.log(result_levels);
+					console.log(result_levels[0]);
 
 					var embed_profile = {
 						title: (String.fromCodePoint(result_levels[0].symbol) +' '+ result_levels[0].title),
