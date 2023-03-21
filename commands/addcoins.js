8a51ece722f596cfd19c1d3ce32e928c1bb88a94
Victor Litvinkov <victor@sunfox.ee>
2023-03-21 22:37:09 +0200

    Addcoins update

--- commands/addcoins.js
+++ commands/addcoins.js
@@ -1,4 +1,14 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const config = require('../config.json');
+const mysql = require('mysql');
+const database = mysql.createConnection({
+    host: config.db_config.host,
+    user: config.db_config.dbuser,
+    password: config.db_config.dbpass,
+    database: config.db_config.dbname,
+    debug: false,
+    multipleStatements: true,
+  });
 
 module.exports = {
 	data: new SlashCommandBuilder()
