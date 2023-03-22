0dd783c0d8b996b6e9e9448e8ce65a9539fafbfd
Victor Litvinkov <victor@sunfox.ee>
2023-03-22 22:29:25 +0200

    Reset interactions file.

--- events/InteractionCreate.js
+++ events/InteractionCreate.js
@@ -3,37 +3,24 @@ const { Events } = require('discord.js');
 module.exports = {
 	name: Events.InteractionCreate,
 	async execute(interaction) {
-        if (interaction.isChatInputCommand()){
-            const command = interaction.client.commands.get(interaction.commandName);
-    
-            if (!command) {
-                console.error(`No command matching ${interaction.commandName} was found.`);
-                return;
-            }
-        
-            try {
-                await command.execute(interaction);
-            } catch (error) {
-                console.error(error);
-                if (interaction.replied || interaction.deferred) {
-                    await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-                } else {
-                    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-                }
-            }
-        } else if (interaction.isButton()){
-            console.log(interaction.customId);  
-            switch (interaction.customId) {
-                case 'run_profile':
-                    var command = 'profile';
-                    await command.execute(interaction);
+        if (!interaction.isChatInputCommand()) return;
 
-                    //await interaction.reply({ content: '/profile', ephemeral: true });
-                    break;
-            
-                default:
-                    break;
-            }          
+        const command = interaction.client.commands.get(interaction.commandName);
+    
+        if (!command) {
+            console.error(`No command matching ${interaction.commandName} was found.`);
+            return;
+        }
+    
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            if (interaction.replied || interaction.deferred) {
+                await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+            } else {
+                await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+            }
         }
 	},
 };
