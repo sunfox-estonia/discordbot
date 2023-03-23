3249a10ec059ae6f838ee72189d75f044ec6460f
Victor Litvinkov <victor@sunfox.ee>
2023-03-23 15:17:20 +0200

    Return Adduser to work.

--- commands/adduser.js
+++ commands/adduser.js
@@ -154,15 +154,15 @@ getProgress = function (user_id, user_level, callback) {
 
 createProfile = function (user_id, callback) {
 	// Prepare MySQL request to retrieve user data	
-	//let sql4 = "INSERT INTO drd_users (uid, level, community) VALUES (?, 0, 'viruviking');"; 
+	let sql4 = "INSERT INTO drd_users (uid, level, community) VALUES (?, 0, 'viruviking');"; 
 	// TODO: Remove community title when database migrates to SQLite  
-	// database.query(sql4, [user_id], (error4, pingback) => {
-	//     if (error4) {
-	//         callback("Ошибка создания профиля пользователя.");
-	//         return;
-	//     } else {
-	callback(null);
-	//     }
-	// });
+	database.query(sql4, [user_id], (error4, pingback) => {
+	    if (error4) {
+	        callback("Ошибка создания профиля пользователя.");
+	        return;
+	    } else {
+			callback(null);
+	    }
+	});
 	// createProfile closed
 }
