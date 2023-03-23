25462f4e414b43863ed4df19d3f19feb18f79949
Victor Litvinkov <victor@sunfox.ee>
2023-03-23 21:39:50 +0200

    Guld Memder add update

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -6,6 +6,5 @@ module.exports = {
     async execute(member) {
         const UserNotify = member.client.channels.cache.get(config.log_channel_id);
         UserNotify.send({content: `Привет, ${member.user}! Рады тебя видеть на нашем сервере :) Если ты используешь ник вместо настоящего имени, пожалуйста смени его на свое имя. Спасибо!`});	
-        console.log(member);
     },
 };
