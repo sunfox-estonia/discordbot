4ddd0c8c0780042f4656d549cdcbbd95c623bd41
Victor Litvinkov <victor@sunfox.ee>
2023-03-25 13:12:19 +0200

    Door olen/close status added.

---
+++ commands/door.js
@@ -0,0 +1,36 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('door')
+		.setDescription('Отправить уведомление об изменении доступности помещения Клуба.')
+		.addStringOption(option =>
+			option.setName('door_status')
+				.setDescription('Статус доступности.')
+				.setRequired(true)
+				.addChoices(
+					{ name: 'open', value: 'Открыт' },
+					{ name: 'close', value: 'Закрыт' }
+				))
+        .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers),
+
+	async execute(interaction) {
+        const door_status = interaction.options.getString('door_status');
+        
+        switch (door_status) {
+            case 'open':
+                var notification_text = (String.fromCodePoint('0x1F511') + ' Двери клуба открыты.');
+                break;
+            case 'close':
+                var notification_text = (String.fromCodePoint('0x1F511') + ' Двери клуба закрыты.');
+                break;
+            default:
+                break;
+        }
+
+        const UserNotify = interaction.client.channels.cache.get(config.log_channel_id);
+		// UserNotify.send({ content: notification_text });
+        // interaction.reply({ content: 'Command has been successfully executed!', ephemeral: true });
+        interaction.reply({ content: notification_text, ephemeral: true });
+	},
+};
