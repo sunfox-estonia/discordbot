fdf828477f43c949b5a38bbbe7c1af2c5a7c7d5d
Victor Litvinkov <victor@sunfox.ee>
2023-08-05 16:03:42 +0300

    Bifrost add user basic functionality.

--- commands/addbifrost.js
+++ commands/addbifrost.js
@@ -47,12 +47,12 @@ module.exports = {
 
                     console.log(">> PROFILE COUNT: "+profile_count);
 
-					if (error || profile_count != "0") {
+					if (error || profile_count > 1) {
 						const locales = {
 							"en-US": 'An error occurred while creating/updating Bifröst profile.',
 							};
 						interaction.reply({ content: locales[interaction.locale] ?? error, ephemeral: true });						
-					}else if (profile_count == "0"){
+					}else if (profile_count == 0){
                         updateBifrost(fetchedUser.user.id, target_steamid, target_xboxid, function(error){
                             if (error) {
                                 const locales = {
