77c6ef0ac618a6c40abd8da8133067e6c8ad1eec
Victor Litvinkov <victor@sunfox.ee>
2023-08-06 00:37:00 +0300

    Add play2gether command for the bot.

--- commands/play2gether.js
+++ commands/play2gether.js
@@ -32,44 +32,70 @@ module.exports = {
             const url_only = interaction.options.getBoolean('link') ?? false;
             const member_id = interaction.member.user.id;
 
-            // Get user Steam and Xbox id from database
-            getBifrost(member_id, function (error, member_data) {
-                if (error) {
-                    const locales = {
-                        "en-US": 'An error occurred while retrieving user profile.'
-                    };
-                    interaction.reply({ content: locales[interaction.locale] ?? error, ephemeral: true });
-                } else {
-                    console.log(member_data);
-                    // Get user data activity from Steam
-                    steam.getUserSummary(member_data.steam_id).then(SteamUser => {
-                        // Check if user is playing something or not
-                        console.log(SteamUser);
+            await interaction.guild.members.fetch(member_id).then(fetchedUser => {
+                var embed_author = fetchedUser.nickname ?? fetchedUser.user.username;
+                // Get user Steam and Xbox id from database
+                getBifrost(member_id, function (error, member_data) {
+                    if (error) {
+                        const locales = {
+                            "en-US": 'An error occurred while retrieving user profile.'
+                        };
+                        interaction.reply({ content: locales[interaction.locale] ?? error, ephemeral: true });
+                    } else {
+                        console.log(member_data);
+                        // Get user data activity from Steam
+                        steam.getUserSummary(member_data.steam_id).then(SteamUser => {
+                            // Check if user is playing something or not
+                            if(SteamUser.gameID !== undefined){
+                                // Get Steam application data
+                                steam.getGameDetails(SteamUser.gameID).then(SteamApp => {                               
+                                    // Generate Bofrost invite URI
+                                    const BofrostUri = 'https://bifrost.snfx.ee/steam/'+SteamApp.steam_appid+'/'+SteamUser.steamID;
+                                    // Show embed component or just URI
+                                    if (url_only === false) {
+                                        var invite_embed = {
+                                            //description: "",
+                                            color: 0xdd6282,
+                                            timestamp: new Date().toISOString(),
+                                            footer: {
+                                                icon_url: "https://sunfox.ee/resources/img/discord_bot/vv_sq_logo.png",
+                                                text: "Викинги Вирумаа"
+                                            },
+                                            thumbnail: {
+                                            url: SteamApp.capsule_image,
+                                            },
+                                            author: {
+                                            name: embed_author + " приглашает поиграть в "+SteamApp.name+".",
+                                            icon_url: "https://cdn.discordapp.com/avatars/"+fetchedUser.user.id+"/"+fetchedUser.user.avatar+".jpeg"
+                                            }
+                                        }
 
-                        if(SteamUser.gameID !== undefined){
-                            // Get Steam application data
-                            steam.getGameDetails(SteamUser.gameID).then(SteamApp => {
-                                console.log(SteamApp);
+                                        const component_buttons = {
+                                            type: 1,
+                                            components: [
+                                                {
+                                                    type: 2,
+                                                    label: "Присоедениться к лобби",
+                                                    style: 5,
+                                                    url: BofrostUri
+                                                }
+                                            ]
+                                        }
 
-                            
-            
-                                // // Generate Bofrost invite URI
-                                // const BofrostUri = 'https://bifrost.snfx.ee/steam/'+SteamApp.gameid+'/'+SteamUser.userid;
-                                // // Show embed component or just URI
-                                // if (url_only === false) {
-
-                                                                        
-                                // } else {
-                                //     interaction.reply({ content: 'Вот Твоя ссылка-приглашение для совестной игры в '+SteamApp.gameid+':\n'+BofrostUri, ephemeral: true });
-                                // }  
-                            });   
-                        } else {
-                            // Show error - no application to invite
-                            interaction.reply({ content: '— Извини, не могу создать приглашение! Убедись что находишься в игровом лобби и попробуй еще раз.', ephemeral: true });
-                        }                        
-                    });
-                }
-            });         
+                                        interaction.reply({embeds: [invite_embed], components: [component_buttons]  });
+                                                                            
+                                    } else {
+                                        interaction.reply({ content: 'Вот Твоя ссылка-приглашение для совестной игры в '+SteamApp.gameid+':\n'+BofrostUri, ephemeral: true });
+                                    }  
+                                });   
+                            } else {
+                                // Show error - no application to invite
+                                interaction.reply({ content: '— Извини, не могу создать приглашение! Убедись что находишься в игровом лобби и попробуй еще раз.', ephemeral: true });
+                            }                        
+                        });
+                    }
+                });    
+            });     
         }
     },
 };
