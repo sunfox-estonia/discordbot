0a0b31a4c0463473b7dc0309297f00023e986805
Victor Litvinkov <victor.litvinkov@sunfox.ee>
2024-03-09 20:39:35 +0200

    Sheduled Events related functionality fix.

--- events/guildSheduledEventCreate.js
+++ events/guildSheduledEventCreate.js
@@ -27,16 +27,12 @@ module.exports = {
         var discord_event_url = event.url;
         var event_role_name = "Участники события: " + discord_event_name;
 
-
-
-
-
-        
         // Step 1. Create a new role for the event
         await event.guild.roles.create({
             name: event_role_name,
             reason: 'Temporary Event-related role. Should be deleted after the event.'
         }).then( NewRole => {
+<<<<<<< HEAD
             console.log(NewRole);
 
             // Step 2. Add the role ID to the Database
@@ -59,5 +55,21 @@ module.exports = {
         // console.log("NEW METHOD");
 
         // console.log(NewRole);
+=======
+
+
+            // Step 2. Add the role ID to the Database
+            let sql1 = "INSERT INTO events_roles (discord_event_id, discord_role_id) VALUES (?, ?)";
+            database.query(sql1, [discord_event_id, NewRole.id], (error1, pingback) => {
+                if (error1) {
+                    console.log(error1);
+                } else {
+                    BotLogChannel.send({ content: `AUTOMATION: Role ${event_role_name} created for event ${discord_event_name}` });
+                    NotificationsChannel.send({ content: `${discord_event_url}` });
+                    callback(null);
+                }
+            })
+        }).catch(err => console.log(err));
+>>>>>>> e6c150c (Sheduled Events related functionality fix.)
     }
 }
