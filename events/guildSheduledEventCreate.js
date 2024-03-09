c5d36950b312768659e2434c9527dd5ccba4a1f9
Victor Litvinkov <victor.litvinkov@sunfox.ee>
2024-03-09 20:44:41 +0200

    Sheduled Events related functionality fix.

--- events/guildSheduledEventCreate.js
+++ events/guildSheduledEventCreate.js
@@ -40,7 +40,6 @@ module.exports = {
                 } else {
                     BotLogChannel.send({ content: `AUTOMATION: Role ${event_role_name} created for event ${discord_event_name}` });
                     NotificationsChannel.send({ content: `${discord_event_url}` });
-                    callback(null);
                 }
             })
         }).catch(err => console.log(err));
