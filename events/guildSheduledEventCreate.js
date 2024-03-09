362eba76857de05a7d293a0726163d70c8b0dc26
Victor Litvinkov <victor.litvinkov@sunfox.ee>
2024-03-09 20:42:22 +0200

    Update the guildSheduledEventCreate.js file to use the new event schema

--- events/guildSheduledEventCreate.js
+++ events/guildSheduledEventCreate.js
@@ -32,32 +32,6 @@ module.exports = {
             name: event_role_name,
             reason: 'Temporary Event-related role. Should be deleted after the event.'
         }).then( NewRole => {
-<<<<<<< HEAD
-            console.log(NewRole);
-
-            // Step 2. Add the role ID to the Database
-
-
-            // console.log(role.id);
-            // let sql1 = "INSERT INTO events_roles (discord_event_id, discord_role_id) VALUES (?, ?)";
-            // database.query(sql1, [discord_event_id, role.id], (error1, pingback) => {
-            //     if (error1) {
-            //         console.log(error1);
-            //     } else {
-            //         BotLogChannel.send({ content: `AUTOMATION: Role ${event_role_name} created for event ${discord_event_name}` });
-            //         NotificationsChannel.send({ content: `${discord_event_url}` });
-            //         callback(null);
-            //     }
-            // })
-        }).catch(err => console.log(err));
-
-        // var NewRole = event.guild.roles.cache.find(role => role.name == event_role_name);
-        // console.log("NEW METHOD");
-
-        // console.log(NewRole);
-=======
-
-
             // Step 2. Add the role ID to the Database
             let sql1 = "INSERT INTO events_roles (discord_event_id, discord_role_id) VALUES (?, ?)";
             database.query(sql1, [discord_event_id, NewRole.id], (error1, pingback) => {
@@ -70,6 +44,5 @@ module.exports = {
                 }
             })
         }).catch(err => console.log(err));
->>>>>>> e6c150c (Sheduled Events related functionality fix.)
     }
 }
