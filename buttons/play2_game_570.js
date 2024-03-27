63af27a37559357b2bc93eaedc3387a40901a6aa
Victor Litvinkov <victor.litvinkov@sunfox.ee>
2024-03-27 20:28:44 +0200

    Added draft code to the button files.

--- buttons/play2_game_570.js
+++ buttons/play2_game_570.js
@@ -1,4 +1,4 @@
-//const {} = require('discord.js');
+const { ChannelType, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
 const config = require('../config.json');
 const mysql = require('mysql');
 const database = mysql.createConnection({
