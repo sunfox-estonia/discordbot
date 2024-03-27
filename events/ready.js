b490cbecefbf20edcd8676afa0e4ac599b7b128f
Victor Litvinkov <victor.litvinkov@sunfox.ee>
2024-03-27 20:18:11 +0200

    Add button files to the bot directory

--- events/ready.js
+++ events/ready.js
@@ -44,11 +44,11 @@ module.exports = {
           var play2_intro_embed = new EmbedBuilder()
           .setColor(0xF28C0F)
           .setTitle( "— Будем играть вместе!" )
-          .setDescription("Приглашай участников сообщества в кооперативные игры с помощью команды `/play2gether`. Доступно два режима использования команды: отдельный для экипажей Sea of Thieves, и для других игр.")
+          .setDescription("Приглашай участников сообщества в кооперативные игры с помощью команды `/play2gether`. Доступно два режима использования команды: для экипажей Sea of Thieves, и для других игр.")
           .setImage("https://r.snfx.ee/img/discord_bot/help/play2gether.gif")
           .addFields(
-              { name: "/play2gehter sot", value: "Выбери цель путешествия, тип судна и голосовой чат для общения с экипажем. После отправки команды, пираты Гильдии получат уведомление, и по возможности присоединятся к Тебе." },
-              { name: "/play2gehter game", value: "Выбери игру из списка и голосовой чат для общения с командой. Участники сервера смогут подключиться к игровому лобби, воспользовавшись сгенерированной ссылкой-приглашением." },
+              { name: "/play2gether sot", value: "Выбери цель путешествия, тип судна и голосовой чат для общения с экипажем. После отправки команды, пираты Гильдии получат уведомление, и по возможности присоединятся к Тебе." },
+              { name: "/play2gether game", value: "Выбери игру из списка и голосовой чат для общения с командой. Участники сервера смогут подключиться к игровому лобби, воспользовавшись сгенерированной ссылкой-приглашением." },
               { name: "\u200b", value: "Используй команду `/play2gether` как показано ниже, либо воспользуйся кнопками для быстрого создания лобби по шаблону." },
           )
           .setFooter({ 
@@ -57,50 +57,50 @@ module.exports = {
           });
 
           // SoT Predefine buttons
-          var buttons_play2_predefined_1 = new ButtonBuilder()
+          var play2_predefined_11 = new ButtonBuilder()
           .setLabel('PVP - Слуги Пламени')
-          .setCustomId('play2_predefine_1')
+          .setCustomId('play2_predefine_sot_1')
           .setEmoji("<:ship_brig:1155489530900660294>")
           .setStyle(ButtonStyle.Secondary);
 
-          var buttons_play2_predefine_2 = new ButtonBuilder()
+          var play2_predefined_12 = new ButtonBuilder()
           .setLabel('PVP - Открытый мир')
-          .setCustomId('play2_predefine_2')
+          .setCustomId('play2_predefine_sot_2')
           .setEmoji("<:ship_sloop:1155489536349057095>")
           .setStyle(ButtonStyle.Secondary);
 
-          var buttons_play2_predefine_3 = new ButtonBuilder()
+          var play2_predefined_13 = new ButtonBuilder()
           .setLabel('Farm - Гильдия')
-          .setCustomId('play2_predefine_3')
+          .setCustomId('play2_predefine_sot_3')
           .setEmoji("<:ship_brig:1155489530900660294>")
           .setStyle(ButtonStyle.Secondary);
 
           // Other games Predefine buttons
-          var buttons_play2_predefine_4 = new ButtonBuilder()
+          var play2_predefined_21 = new ButtonBuilder()
           .setLabel('Deep Rock Galactic')
-          .setCustomId('play2_predefine_4')
+          .setCustomId('play2_predefine_game_548430')
           .setStyle(ButtonStyle.Secondary);
 
-          var buttons_play2_predefine_5 = new ButtonBuilder()
+          var play2_predefined_22 = new ButtonBuilder()
           .setLabel('Lethal Company')
-          .setCustomId('play2_predefine_5')
+          .setCustomId('play2_predefine_game_1966720')
           .setStyle(ButtonStyle.Secondary);
 
-          var buttons_play2_predefine_6 = new ButtonBuilder()
+          var play2_predefined_23 = new ButtonBuilder()
           .setLabel('Dota 2')
-          .setCustomId('play2_predefine_6')
+          .setCustomId('play2_predefine_game_570')
           .setStyle(ButtonStyle.Secondary);
 
-          var buttons_play2_predefine_7 = new ButtonBuilder()
+          var play2_predefined_24 = new ButtonBuilder()
           .setLabel('RUST')
-          .setCustomId('play2_predefine_7')
+          .setCustomId('play2_predefine_game_252490')
           .setStyle(ButtonStyle.Secondary);
 
           var play2_buttons_row_1 = new ActionRowBuilder()
-              .addComponents(buttons_play2_predefine_2, buttons_play2_predefined_1, buttons_play2_predefine_3);
+              .addComponents(play2_predefined_12, play2_predefined_11, play2_predefined_13);
 
           var play2_buttons_row_2 = new ActionRowBuilder()
-              .addComponents(buttons_play2_predefine_4, buttons_play2_predefine_5, buttons_play2_predefine_6, buttons_play2_predefine_7);
+              .addComponents(play2_predefined_21, play2_predefined_22, play2_predefined_23, play2_predefined_24);
 
           Play2Channel.send({ embeds: [play2_intro_embed], components: [play2_buttons_row_1, play2_buttons_row_2] });
 	},
